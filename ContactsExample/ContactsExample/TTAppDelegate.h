368b5f513731a27cddd5e202f485b320d04d4c43
extso <extso@outlook.com>
2014-08-30 14:46:26 +0530

    Project Commit

---
+++ ContactsExample/ContactsExample/TTAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  TTAppDelegate.h
+//  ContactsExample
+//
+//  Created by TheTiger on 27/08/14.
+//  Copyright (c) 2014 TheTiger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) UINavigationController *navigationController;
+
+@end
---
+++ ContactsExample/ContactsExample/TTViewController.h
@@ -0,0 +1,13 @@
+//
+//  TTViewController.h
+//  ContactsExample
+//
+//  Created by TheTiger on 27/08/14.
+//  Copyright (c) 2014 TheTiger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTViewController : UIViewController
+
+@end
